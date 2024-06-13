a9e673762509660357f97655a99c3e6a38f96050
shiva vardhan <110530841+shiva6242@users.noreply.github.com>
2024-06-13 12:19:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,19 +29,7 @@ const connect = () => {
 // Middleware
 const allowedOrigins = ['https://papaya-baklava-8b0fd2.netlify.app'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST','PUT'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true,
-}));
+app.use(cors());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cookieParser());
