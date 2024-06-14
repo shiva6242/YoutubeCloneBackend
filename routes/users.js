f10b6c3cb1b12ff7c096bef3a9c7cee7c880f412
shiva vardhan <110530841+shiva6242@users.noreply.github.com>
2024-06-14 10:35:05 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -14,24 +14,24 @@ const { verifyToken }=require('../verifyToken.js');
 
 
 //update user
-router.put("/:id",verifyToken, update);
+router.put("/:id", update);
 
 //delete user
-router.delete("/:id",verifyToken, deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 //get a user
-router.get("/find/:id",verifyToken, getUser);
+router.get("/find/:id", getUser);
 
 //subscribe a user
-router.put("/sub/:id",verifyToken, subscribe);
+router.put("/sub/:id",  subscribe);
 
 //unsubscribe a user
-router.put("/unsub/:id",verifyToken, unsubscribe);
+router.put("/unsub/:id",  unsubscribe);
 
 //like a video
-router.put("/like/:videoId",verifyToken,  like);
+router.put("/like/:videoId",like);
 
 //dislike a video
-router.put("/dislike/:videoId",verifyToken,  dislike);
+router.put("/dislike/:videoId", dislike);
 
 module.exports= router;
